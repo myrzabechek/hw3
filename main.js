d97dd39263690d94c40e2007a6758c9a38f77e20
Rufiyy <r96000850@gmail.com>
2024-10-09 18:25:23 +0600

    dz3

---
+++ main.js
@@ -0,0 +1,33 @@
+var bigNum = ( num1 , num2) => {
+    if (num1 < num2){
+        console.log(num1 +" " + " Это меньше")
+    }else{
+        console.log(num2 + " " + " Это меньше")
+    }
+}
+bigNum( 6,5 )
+
+var textRequest = prompt(" Введите текст")
+var texstLength = (data) => {
+    console.log(data.length)
+}
+texstLength(textRequest)
+
+var calculator = (num1, num2, operation) => {
+    if (operation === "+") {
+        console.log(num1 + num2)
+    }else if (operation === "-") {
+        console.log(num1 - num2)
+    }else if (operation === "*") {
+        console.log(num1 * num2)
+    }else if (operation === "/") {
+        if(num2 === 0) {
+            console.log("error")
+        }else{
+            console.log(num1 / num2)
+        }
+    }
+}
+calculator( 10,0,"/");
+
+
